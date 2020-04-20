55ccdec4bc92db114776df1e9588b17f6d6fd638
realgangsterz <44780068+realgangsterz@users.noreply.github.com>
2020-04-20 17:17:38 +0700

    complete FizzBuzz

--- TestFirst_TDD/Fizz-Buzz/src/FizzBuzzTranslate.java
+++ TestFirst_TDD/Fizz-Buzz/src/FizzBuzzTranslate.java
@@ -1,4 +1,82 @@
-package PACKAGE_NAME;
-
 public class FizzBuzzTranslate {
+    public static String translate(int number){
+        boolean isFizz = (number % 3 == 0 && number % 5 != 0);
+        boolean isBuzz = (number % 5 == 0 && number % 3 != 0);
+        boolean isFizzBuzz = (number % 3 == 0 && number % 5 == 0);
+
+        if (isFizz)
+            return "Fizz";
+        else if (isBuzz)
+            return "Buzz";
+        else if (isFizzBuzz)
+            return "FizzBuzz";
+        else{
+            if (number > 0 && number < 100)return readNum(number);
+            else return "Khong doc duoc";
+        }
+    }
+    public static String readNum(int number){
+        String read = "";
+        int ones = number % 10, tens = number / 10;
+        switch (tens){
+            case 1:
+                read += "mot";
+                break;
+            case 2:
+                read += "hai";
+                break;
+            case 3:
+                read += "ba";
+                break;
+            case 4:
+                read += "bon";
+                break;
+            case 5:
+                read += "nam";
+                break;
+            case 6:
+                read += "sau";
+                break;
+            case 7:
+                read += "bay";
+                break;
+            case 8:
+                read += "tam";
+                break;
+            case 9:
+                read += "chin";
+                break;
+        }
+        read += " ";
+        switch (ones){
+            case 1:
+                read += "mot";
+                break;
+            case 2:
+                read += "mot";
+                break;
+            case 3:
+                read += "ba";
+                break;
+            case 4:
+                read += "bon";
+                break;
+            case 5:
+                read += "nam";
+                break;
+            case 6:
+                read += "sau";
+                break;
+            case 7:
+                read += "bay";
+                break;
+            case 8:
+                read += "tam";
+                break;
+            case 9:
+                read += "chin";
+                break;
+        }
+        return read;
+    }
 }
--- TestFirst_TDD/Fizz-Buzz/test/FizzBuzzTranslateTest.java
+++ TestFirst_TDD/Fizz-Buzz/test/FizzBuzzTranslateTest.java
@@ -1,5 +1,47 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTranslateTest {
 
+    @Test
+    void testTranslateFizz() {
+        int number = 3;
+
+        String expStr = "Fizz";
+        String result = FizzBuzzTranslate.translate(number);
+        assertEquals(expStr,result);
+    }
+    @Test
+    void testTranslateBuzz() {
+        int number = 5;
+
+        String expStr = "Buzz";
+        String result = FizzBuzzTranslate.translate(number);
+        assertEquals(expStr,result);
+    }
+    @Test
+    void testTranslateReadNum() {
+        int number = 23;
+
+        String expStr = "hai ba";
+        String result = FizzBuzzTranslate.translate(number);
+        assertEquals(expStr,result);
+    }
+    @Test
+    void testTranslateFizzBuzz() {
+        int number = 15;
+
+        String expStr = "FizzBuzz";
+        String result = FizzBuzzTranslate.translate(number);
+        assertEquals(expStr,result);
+    }
+    @Test
+    void testTranslateCannotReadNum() {
+        int number = 121;
+
+        String expStr = "Khong doc duoc";
+        String result = FizzBuzzTranslate.translate(number);
+        assertEquals(expStr,result);
+    }
 }
